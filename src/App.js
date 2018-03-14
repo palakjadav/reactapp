490f3d9b3f3479c9ff06b71a19c9b32437766eb7
Palak Jadav <palaknjadav@gmail.com>
2018-03-14 15:13:17 -0400

    prescription image thumbnail

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
+constructor(props) {
     super(props);
-    this.state = {value: ''};
+    //this.state = {file: '',imagePreviewUrl: ''};
+    this.state = {file: '',imagePreviewUrl: '',value: ''};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -20,25 +21,74 @@ class App extends Component {
     console.log('A name was submitted: ' + this.state.value);
     event.preventDefault();
   }
+  handleUpload(e) {
+    e.preventDefault();
+    // TODO: do something with -> this.state.file
+    console.log('handle uploading-', this.state.file);
+  }
+
+  handleImageChange(e) {
+    e.preventDefault();
+
+    let reader = new FileReader();
+    let file = e.target.files[0];
+
+    reader.onloadend = () => {
+      this.setState({
+        file: file,
+        imagePreviewUrl: reader.result
+      });
+    }
+
+    reader.readAsDataURL(file)
+  }
 
   render() {
+    let {imagePreviewUrl} = this.state;
+    let $imagePreview = null;
+    if (imagePreviewUrl) {
+      $imagePreview = (<img src={imagePreviewUrl} />);
+    } 
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <br/><br/>
-       <form onSubmit={this.handleSubmit}>
-        <label>
-          Enter Your Name:
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+          <br/><br/>
+        <div className="first-div">
+          <form onSubmit={this.handleSubmit}>
+          <label>
+            Enter Your Name:
+            
+          </label>
           <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <br/><br/>
-        <input type="submit" value="Submit" />
-      </form>
+          <br/><br/>
+          <input type="submit" value="Submit" />
+          </form>
+        </div>
+
+        <div className="second-div">
+          <form onSubmit={(e)=>this.handleUpload(e)}>
+            
+            <label> Upload Prescription: </label>
+            <input className="fileInput" 
+              type="file" 
+              onChange={(e)=>this.handleImageChange(e)} />
+
+            <button className="uploadButton" 
+              type="submit" 
+              onClick={(e)=>this.handleUpload(e)}>Upload Image</button>
+
+            <div className="imgPreview">
+              {$imagePreview}
+            </div>
+          </form>
+          
+        </div>
+        
       </div>
-    );
+    )
   }
 }
 
